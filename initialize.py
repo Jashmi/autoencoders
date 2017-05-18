216934095dea607d4a003443919a4f1c38525312
Jashmi Lagisetty <jlagiset@asu.edu>
2017-05-18 11:04:52 -0700

    Update initialize.py

--- initialize.py
+++ initialize.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
 					"disp_flag"       : True
 					
 					}
-					
+	#demo 1 (no. of layers=2, learning rate = 0.4, activation func = Sigmoid) DEFAULT CASE				
 	build_params = {
 					"learning_rate"  :	0.4,
 					"n_hidden_enc"   :  [ 256 ],
@@ -51,7 +51,36 @@ if __name__ == '__main__':
 					"end_mom"		 :  0.85,
 					"mom_thrs"		 :  100
 					}
-					
+	
+	#demo 2 (no. of layers=4, learning rate = 0.4, activation func = Sigmoid) 
+	# build_params = {
+	# 				"learning_rate"  :	0.4,
+	# 				"n_hidden_enc"   :  [ 256, 128 ],
+	# 				"n_hidden_dec"   :  [ 128, 256 ],
+	# 				"activation"	 :	[ Sigmoid, Sigmoid ], 	
+	# 				"cost_fun"		 :	'rmse',
+	# 				"tied_weights"	 :  True,
+	# 				"LR_decay"		 :  0.0007,
+	# 				"begin_mom"		 :  0.4,
+	# 				"end_mom"		 :  0.85,
+	# 				"mom_thrs"		 :  200
+	# 				}
+
+	#demo 3 (no. of layers=4, learning rate = 0.5, activation func = Sigmoid) 
+	# build_params = {
+	# 				"learning_rate"  :	0.5,
+	# 				"n_hidden_enc"   :  [ 512, 256 ],
+	# 				"n_hidden_dec"   :  [ 256, 512 ],
+	# 				"activation"	 :	[ Sigmoid, Sigmoid ], 	
+	# 				"cost_fun"		 :	'rmse',
+	# 				"tied_weights"	 :  True,
+	# 				"LR_decay"		 :  0.0007,
+	# 				"begin_mom"		 :  0.4,
+	# 				"end_mom"		 :  0.85,
+	# 				"mom_thrs"		 :  200
+	# 				}
+				
+				
 	train_params = {
 					"training_epochs" : 200
 					}
